b9540ea59a1a951c0882f7a166ddc3bf6fc674a1
Michael Vu <michael.vu.97@outlook.com>
2018-04-03 15:55:56 -0400

    added support for multiple scenes

--- ASST3/main.cpp
+++ ASST3/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[])
 	// assignment.  
 	Raytracer raytracer;
 	LightList light_list;
-	Scene scene;   
+	Scene scene1, sceneMirrorRoom;   
 
 	int width = 320;
 	int height = 240;
@@ -79,110 +79,124 @@ int main(int argc, char* argv[])
 	/*
 	 * PRETTY SCENE
 	 */
-	// SceneNode* sphere = new SceneNode(new UnitSphere(), &glass, 0.1);
-	// scene.push_back(sphere);
-	// SceneNode* sphere2 = new SceneNode(new UnitSphere(), &gold, 0.1);
-	// scene.push_back(sphere2);
-	// SceneNode* sphere3 = new SceneNode(new UnitSphere(), &gold, 0.1);
-	// scene.push_back(sphere3);
-	// SceneNode* sphere4 = new SceneNode(new UnitSphere(), &mirror, 0.1);
-	// scene.push_back(sphere4);
-	// SceneNode* sphere5 = new SceneNode(new UnitSphere(), &mirror, 0.1);
-	// scene.push_back(sphere5);
-	// SceneNode* plane = new SceneNode(new UnitSquare(), &jade, 0);
-	// scene.push_back(plane);
+	if (true) { // Isolate local variables from other scenes.
+		SceneNode* sphere = new SceneNode(new UnitSphere(), &glass, 0.1);
+		scene1.push_back(sphere);
+		SceneNode* sphere2 = new SceneNode(new UnitSphere(), &gold, 0.1);
+		scene1.push_back(sphere2);
+		SceneNode* sphere3 = new SceneNode(new UnitSphere(), &gold, 0.1);
+		scene1.push_back(sphere3);
+		SceneNode* sphere4 = new SceneNode(new UnitSphere(), &mirror, 0.1);
+		scene1.push_back(sphere4);
+		SceneNode* sphere5 = new SceneNode(new UnitSphere(), &mirror, 0.1);
+		scene1.push_back(sphere5);
+		SceneNode* plane = new SceneNode(new UnitSquare(), &jade, 0);
+		scene1.push_back(plane);
 
-	// SceneNode* plane2 = new SceneNode(new UnitSquare(), &bloo, 0);
-	// scene.push_back(plane2);
+		SceneNode* plane2 = new SceneNode(new UnitSquare(), &bloo, 0);
+		scene1.push_back(plane2);
 
-	// double factor1[3] = { 1.0, 2.0, 1.0 };
+		double factor1[3] = { 1.0, 2.0, 1.0 };
+		double factor2[3] = { 6.0, 6.0, 6.0 };
+		double factor3[3] = { 2.0, 2.0, 2.0 };
 
-	// double factor3[3] = { 2.0, 2.0, 2.0 };
-	// sphere->translate(Vector3D(0, 0, -3));
+		sphere->translate(Vector3D(0, 0, -3));
 
-	// sphere2->translate(Vector3D(-1.0, -1.0, -5));
+		sphere2->translate(Vector3D(-1.0, -1.0, -5));
 
-	// sphere3->translate(Vector3D(-1.0, 1.0, -5));
+		sphere3->translate(Vector3D(-1.0, 1.0, -5));
 
-	// sphere4->translate(Vector3D(1.0, -1.0, -5));
-	// sphere5->translate(Vector3D(1.0, 1.0, -5));
+		sphere4->translate(Vector3D(1.0, -1.0, -5));
+		sphere5->translate(Vector3D(1.0, 1.0, -5));
 
-	// double factor2[3] = { 6.0, 6.0, 6.0 };
 
-	// plane->translate(Vector3D(0, 0, -7));
-	// plane->rotate('z', 45);
-	// plane->scale(Point3D(0, 0, 0), factor2);
+		plane->translate(Vector3D(0, 0, -7));
+		plane->rotate('z', 45);
+		plane->scale(Point3D(0, 0, 0), factor2);
 
-	// plane2 -> translate(Vector3D(-2, -2, -7 + 3));
-	// plane2 -> rotate('y', 90);
-	// plane2 -> rotate('x', -45);
-	// plane2 -> scale(Point3D(0,0,0), factor2);
+		plane2 -> translate(Vector3D(-2, -2, -7 + 3));
+		plane2 -> rotate('y', 90);
+		plane2 -> rotate('x', -45);
+		plane2 -> scale(Point3D(0,0,0), factor2);
+	}
 	/*
 	 * END PRETTY SCENE
 	 */
 
-	SceneNode* sphere = new SceneNode(new UnitSphere(), &glass, 0.1);
-	scene.push_back(sphere);
+	/*
+	 * MIRROR ROOM SCENE
+	 */
+	if (true) { // Isolate local variables from other scenes.
+		SceneNode* sphere = new SceneNode(new UnitSphere(), &glass, 0.1);
+		sceneMirrorRoom.push_back(sphere);
 
-	SceneNode* plane = new SceneNode(new UnitSquare(), &mirror, 0);
-	scene.push_back(plane);
-	SceneNode* plane2 = new SceneNode(new UnitSquare(), &mirror, 0);
-	scene.push_back(plane2);
-	SceneNode* plane3 = new SceneNode(new UnitSquare(), &mirror, 0);
-	scene.push_back(plane3);
-	SceneNode* plane4 = new SceneNode(new UnitSquare(), &mirror, 0);
-	scene.push_back(plane4);
+		SceneNode* plane = new SceneNode(new UnitSquare(), &mirror, 0);
+		sceneMirrorRoom.push_back(plane);
+		SceneNode* plane2 = new SceneNode(new UnitSquare(), &mirror, 0);
+		sceneMirrorRoom.push_back(plane2);
+		SceneNode* plane3 = new SceneNode(new UnitSquare(), &mirror, 0);
+		sceneMirrorRoom.push_back(plane3);
+		SceneNode* plane4 = new SceneNode(new UnitSquare(), &mirror, 0);
+		sceneMirrorRoom.push_back(plane4);
 
-	SceneNode* plane5 = new SceneNode(new UnitSquare(), &mirror, 0);
-	scene.push_back(plane5);
+		SceneNode* plane5 = new SceneNode(new UnitSquare(), &mirror, 0);
+		sceneMirrorRoom.push_back(plane5);
 
-	SceneNode* plane6 = new SceneNode(new UnitSquare(), &mirror, 0);
-	scene.push_back(plane6);
+		SceneNode* plane6 = new SceneNode(new UnitSquare(), &mirror, 0);
+		sceneMirrorRoom.push_back(plane6);
 
 
 
-	double factor1[3] = {6, 6, 6};
-	double factor2[3] = {10, 6, 1};
-	plane -> translate(Vector3D(0, 0, -8));
-	plane -> scale (Point3D(0, 0, 0), factor1);
+		double factor4[3] = {6, 6, 6};
+		double factor5[3] = {10, 6, 1};
+		plane -> translate(Vector3D(0, 0, -8));
+		plane -> scale (Point3D(0, 0, 0), factor4);
 
-	sphere -> translate(Vector3D(0, 0, -3));
+		sphere -> translate(Vector3D(0, 0, -3));
 
-	plane2 -> translate(Vector3D(0, 0, 2));
-	plane2 -> scale(Point3D(0,0,0), factor1);
+		plane2 -> translate(Vector3D(0, 0, 2));
+		plane2 -> scale(Point3D(0,0,0), factor4);
 
-	plane3 -> translate(Vector3D(-3, 0, -5));
-	plane3 -> rotate('y', 90);
-	plane3 -> scale(Point3D(0,0,0), factor2);
+		plane3 -> translate(Vector3D(-3, 0, -5));
+		plane3 -> rotate('y', 90);
+		plane3 -> scale(Point3D(0,0,0), factor5);
 
-	plane4 -> translate(Vector3D(3, 0, -5));
-	plane4 -> rotate('y', -90);
-	plane4 -> scale(Point3D(0,0,0), factor2);
-	
-	plane5 -> translate(Vector3D(0, 3, -5));
-	plane5 -> rotate('x', 90);
-	plane5 -> scale(Point3D(0,0,0), factor2);
+		plane4 -> translate(Vector3D(3, 0, -5));
+		plane4 -> rotate('y', -90);
+		plane4 -> scale(Point3D(0,0,0), factor5);
+		
+		plane5 -> translate(Vector3D(0, 3, -5));
+		plane5 -> rotate('x', 90);
+		plane5 -> scale(Point3D(0,0,0), factor5);
 
-	plane6 -> translate(Vector3D(0, -3, -5));
-	plane6 -> rotate('x', -90);
-	plane6 -> scale(Point3D(0,0,0), factor2);
+		plane6 -> translate(Vector3D(0, -3, -5));
+		plane6 -> rotate('x', -90);
+		plane6 -> scale(Point3D(0,0,0), factor5);
+	}
+	/*
+	 * END MIRROR ROOM SCENE
+	 */
 
 	// Render the scene, feel free to make the image smaller for
 	// testing purposes.	
 	Camera camera1(Point3D(0, 0, 1), Vector3D(0, 0, -1), Vector3D(0, 1, 0), 60.0);
 	Image image1(width, height);
-	raytracer.render(camera1, scene, light_list, image1); //render 3D scene to image
+	raytracer.render(camera1, scene1, light_list, image1); //render 3D scene to image
 	image1.flushPixelBuffer("view1.bmp"); //save rendered image to file
 
 	// Render it from a different point of view.
 	Camera camera2(Point3D(4, 2, 1), Vector3D(-4, -2, -6), Vector3D(0, 1, 0), 60.0);
 	Image image2(width, height);
-	raytracer.render(camera2, scene, light_list, image2);
+	raytracer.render(camera2, scene1, light_list, image2);
 	image2.flushPixelBuffer("view2.bmp");
 
 	// Free memory
-	for (size_t i = 0; i < scene.size(); ++i) {
-		delete scene[i];
+	for (size_t i = 0; i < scene1.size(); ++i) {
+		delete scene1[i];
+	}
+
+	for (size_t i = 0; i < sceneMirrorRoom.size(); ++i) {
+		delete sceneMirrorRoom[i];
 	}
 
 	for (size_t i = 0; i < light_list.size(); ++i) {
